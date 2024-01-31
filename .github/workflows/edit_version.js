11cdf5448e9ff3022341c835fb92d2e59705f7c9
NXY <62371554+nxy666@users.noreply.github.com>
2024-01-31 21:55:10 +0800

    尝试主动向gitee提交分支

---
+++ .github/workflows/edit_version.js
@@ -0,0 +1,37 @@
+﻿const fs = require('fs');
+const exec = require("child_process");
+
+(async () => {
+	// 安装依赖
+	exec.execSync('npm install fast-xml-parser');
+	
+	const {XMLParser, XMLBuilder} = require('fast-xml-parser');
+	
+	const {PUBLISH_VERSION} = process.argv;
+
+	const csprojPath = './PolicyManager.csproj';
+	const csprojData = fs.readFileSync(csprojPath, 'utf-8');
+
+	// 解析XML
+	const options = {
+		ignoreAttributes: false,
+		attributeNamePrefix: '@',
+		suppressEmptyNode: true,
+		format: true
+	};
+
+	const parser = new XMLParser(options);
+	const csprojObj = parser.parse(csprojData);
+
+	// 修改版本号
+	if (csprojObj.Project?.PropertyGroup?.[0]) {
+		csprojObj.Project.PropertyGroup[0].Version = PUBLISH_VERSION;
+	}
+
+	// 转换为XML字符串
+	const builder = new XMLBuilder(options);
+	const xml = builder.build(csprojObj);
+
+	// 写回.csproj文件
+	fs.writeFileSync(csprojPath, xml);
+})();
