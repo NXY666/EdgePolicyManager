22e43b33632c58001d223c964242503133951a3f
NXY <62371554+nxy666@users.noreply.github.com>
2024-12-01 22:24:12 +0800

    简化自动化发布的修改版本逻辑

--- .github/workflows/edit_version.mjs
+++ .github/workflows/edit_version.mjs
@@ -1,39 +1,12 @@
 ﻿import fs from 'fs';
-import {XMLParser, XMLBuilder} from 'fast-xml-parser';
 
 (async () => {
 	const {PUBLISH_VERSION, CONFIG} = process.env;
 
 	const csprojPath = './PolicyManager.csproj';
-	const csprojData = fs.readFileSync(csprojPath);
+	const csprojData = fs.readFileSync(csprojPath).toString();
 
-	// 解析XML
-	const options = {
-		ignoreAttributes: false,
-		attributeNamePrefix: '@',
-		suppressEmptyNode: true,
-		format: CONFIG === 'Debug'
-	};
-
-	const parser = new XMLParser(options);
-	const csprojObj = parser.parse(csprojData);
-
-	// 修改版本号
-	if (csprojObj.Project?.PropertyGroup) {
-		const config = csprojObj.Project.PropertyGroup.find((item) => item["Version"]);
-		if (!config) {
-			console.log(JSON.stringify(csprojObj.Project.PropertyGroup));
-			throw new Error('Cannot find version property.');
-		}
-		config["Version"] = PUBLISH_VERSION;
-	} else {
-		console.log(JSON.stringify(csprojObj));
-		throw new Error('Cannot find PropertyGroup.');
-	}
-
-	// 转换为XML字符串
-	const builder = new XMLBuilder(options);
-	const xml = builder.build(csprojObj);
+	const xml = csprojData.replace("0.0.0.0", PUBLISH_VERSION);
 
 	// 写回.csproj文件
 	fs.writeFileSync(csprojPath, xml);
